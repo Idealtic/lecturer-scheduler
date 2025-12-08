bbff6fb6eb5339e73ba9f34322db61699dee2264
Idealtic <hoangphamminh85@gmail.com>
2025-12-08 07:39:02 +0700

    update

--- database/database.py
+++ database/database.py
@@ -1,11 +1,9 @@
 import sqlite3
 import os
 
-DB_PATH = "database/university.db"
+DB_PATH = "university.db"
 
 def get_connection(): #hàm khởi tạo
-    if not os.path.exists("database"):
-        os.makedirs("database")
     conn = sqlite3.connect(DB_PATH)
     conn.row_factory = sqlite3.Row
     conn.execute("PRAGMA foreign_keys = ON")
