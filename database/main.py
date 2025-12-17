a241fc2f907bac8904e4790943e9910f541666cf
Ela-69 <duongtunglamhg@gmail.com>
2025-12-17 08:02:50 +0700

    Update main.py

--- database/main.py
+++ database/main.py
@@ -5,6 +5,7 @@ from database import (
     clear_schedule
 )
 from assigner import solve_and_record
+from sample_data import create_sample_data
 
 if __name__ == "__main__":
 
@@ -15,14 +16,14 @@ if __name__ == "__main__":
 
         # 2. Xóa lịch AUTO cũ để chạy mới
         clear_schedule(conn)
+        create_sample_data(conn)
 
         # 3. Chạy thuật toán phân công
         assignments = solve_and_record(conn, commit_result=True)
-
+        #print("debugg", assignments)
         # 4. In lịch đầy đủ sau khi phân công
         print("=== FULL SCHEDULE VIEW ===")
         full_schedule = get_full_schedule_view(conn)
-
         for row in full_schedule:
             print(
                 f"Class {row['class_id']}: {row['subject_name']}, "
